168ce5be82f552efb621ba8198f5d0cd3a933d39
wendercorrea <wendercorrea@gmail.com>
2025-03-11 19:30:51 -0300

    ACF1995WP

--- src/services/BibleTextService.ts
+++ src/services/BibleTextService.ts
@@ -8,7 +8,7 @@ export class BibleTextService {
     }
 
     try {
-      const response = await fetch('KJF.json');
+      const response = await fetch('ACF1995WP.json');
       if (!response.ok) {
         throw new Error('Failed to fetch Bible data');
       }
