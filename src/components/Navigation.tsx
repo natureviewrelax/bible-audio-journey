1734174434017ec40b03f1f3c1f33da54d927cef
wendercorrea <wendercorrea@gmail.com>
2025-03-28 13:53:04 -0300

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -163,8 +163,7 @@ export const Navigation = ({
                    </Link>
                   </DropdownMenuItem>
                 </>
-              )}
-              {user && (
+              ) : (user && (
                 <>
                   <DropdownMenuItem asChild>
                     <Link to="/profile" className="flex items-center cursor-pointer hover:bg-accent/50 rounded-lg transition-all duration-200 px-3 py-2 mb-1 group">
@@ -177,11 +176,13 @@ export const Navigation = ({
                     <span className="font-medium">Sair</span>
                   </DropdownMenuItem>
                 </>
-              )}
+              ))}
+              
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
       </div>
+
       <div className="flex items-center gap-4">
         <Button 
           variant="outline" 
